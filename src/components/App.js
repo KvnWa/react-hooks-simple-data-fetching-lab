f4f309690e0c3d0178a59c1a9e6b05cc6b605ffd
Kevin Wang <kevqwang@gmail.com>
2022-02-05 23:00:16 -0500

    commit tree 1df25ed4753e145e44f0f2b584fca76bdb878eec

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+const api = 'https://dog.ceo/api/breeds/image/random'
+
+function App() {
+    const [ image, setImage ] = useState(null)
+
+    useEffect(() => {
+        fetch(api)
+        .then(res => res.json())
+        .then(data => setImage(data.message))
+    }, [])
+
+if (!image) {
+    return <h2>Loading...</h2>
+}
+
+  return (
+  <div>
+     <img alt="A Random Dog" src={image}></img>
+  </div>
+  )
+}
+
+export default App;
 // create your App component here
